056c6db876c61cf3c0022f178f0e05ab39bce286
Christina Starr <christinastarr19@gmail.com>
2020-06-15 20:03:14 -0400

    added screenshot to README, linked local storage to display info

--- Assets/script.js
+++ Assets/script.js
@@ -4,81 +4,64 @@
 //add types to the columns:
 
 $(document).ready(function () {
-  hours = [9, 10, 11, 12, 1, 2, 3, 4, 5];
-  hoursIndex = 0;
-  var currentTime = moment(moment());
-  console.log(currentTime);
 
-  
-  var newRow = $("<div>").addClass("row");
-  $(".container").append(newRow);
+  hoursObject = [
+    { twelveHour: "9 AM", twentyFourHour: "9" },
+    { twelveHour: "10 AM", twentyFourHour: "10" },
+    { twelveHour: "11 AM", twentyFourHour: "11" },
+    { twelveHour: "12 PM", twentyFourHour: "12" },
+    { twelveHour: "1 PM", twentyFourHour: "13" },
+    { twelveHour: "2 PM", twentyFourHour: "14" },
+    { twelveHour: "3 PM", twentyFourHour: "15" },
+    { twelveHour: "4 PM", twentyFourHour: "16" },
+    { twelveHour: "5 PM", twentyFourHour: "17" },
+  ];
 
-  for (var i = 0; i < 3; i++) {
-    createAMTimeBlocks();
-  };
-  for (var i = 0; i < 6; i++){
-      createPMTimeBlocks();
+  $("#currentDay").text(moment().format("MMMM Do YYYY"));
+
+  for (var i = 0; i < 9; i++) {
+    createTimeBlocks(i);
   }
-  function createAMTimeBlocks() {
+
+  function createTimeBlocks(index) {
+    var newRow = $("<div>").addClass("row");
     var column1 = $("<div>")
       .addClass("col-sm-1 hour")
-      .text(hours[hoursIndex] + "AM");
+      .text(hoursObject[index].twelveHour);
     console.log("This is the first column.");
-    var column2 = $("<textarea>").addClass("col-sm-10  past present future");
+    var column2 = $("<textarea>").addClass("col-sm-10 input").val(localStorage.getItem(hoursObject[index].twelveHour));
     var column3 = $("<button>")
       .addClass("col-sm-1 saveBtn")
       .html("<i class='fas fa-save'></i>");
     //Append to existing element
-    $(".row").append(column1);
-    $(".row").append(column2);
-    $(".row").append(column3);
-    hoursIndex++
-  };
-
-  function createPMTimeBlocks() {
-      
-      var column1 = $("<div>")
-        .addClass("col-sm-1 hour")
-        .text(hours[hoursIndex] + "PM");
-      console.log("This is the first column.");
-      var column2 = $("<textarea>").addClass("col-sm-10 past present future description");
-      var column3 = $("<button>")
-        .addClass("col-sm-1 saveBtn")
-        .html("<i class='fas fa-save'></i>");
-      //Append to existing element
-      $(".row").append(column1);
-      $(".row").append(column2);
-      $(".row").append(column3);
-      hoursIndex++
-    };
- function setColor() {
-     if (hours[hoursIndex] === (moment())){
-         $("textarea").addClass("past")
-     } else if (hours[hoursIndex] > (moment())) {
-        $("textarea").addClass("past")
-     }
+    newRow.append(column1).append(column2).append(column3);
+    $(".container").append(newRow);
+    if (parseInt(hoursObject[index].twentyFourHour) === parseInt(moment().format("H"))) {
+      column2.addClass("present");
+    } else if (parseInt(hoursObject[index].twentyFourHour) > parseInt(moment().format("H"))) {
+      column2.addClass("future");
+    } else {
+      column2.addClass("past");
     }
-    setColor();
-    console.log("Testing")
+    //hoursIndex++;
+  }
+
+  
+
+  
+
+  $(".container").on("click", ".saveBtn", function (event) {
+    var input = $(this).siblings(".input").val();
+    var time = $(this).siblings(".hour").text();
+    console.log(time);
+    //save todo and time in local storage
+    localStorage.setItem(time, input);
+    // localStorage.setItem("myList", JSON.stringify({toDo: input, time: time}));
+    console.log();
+     
+    
+    
+
+    //   }
+  });
 });
-
-
-//Vol's Ideas
-//   var hours = [
-//     "9AM",
-//     "10AM",
-//     "11AM",
-//     "12PM",
-//     "1PM",
-//     "2PM",
-//     "3PM",
-//     "4PM",
-//     "5PM",
-//   ];
-//   for (var i = 0; i < 9; i++) {
-//     var newRow = $(
-//       `<div class='row'><span class='col-sm-1 hour'></span><textarea class='col-sm-10 present past future'></textarea><div class='col-sm-1 saveBtn'><i class="far fa-save"></i></div></div>`
-//     );
-//     $("span").text(hours[i]);
-//     $(".container").append(newRow);
-//   }
--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# gt-scheduler-third-party-apis-homework-05
+# Daily Planner using APIs
+
+## Table of Contents 
+
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [MIT_License](#mit_license)
+
+
+
+## Installation
+
+1. Clone the repo and 
+2. Run the index.html file in default browser.
+
+
+## Usage 
+
+This daily scheduler was designed to run in the browser. It retrieves information from Moment.js, a third party api, and uses its' data to give color-coded information. The user's input saves to local storage which updates the page in real time. It was also designed to meet the criteria for GT Full-stack Flex program homework requirements. 
+
+It is built using DOM manipulation techniques in Javascript and customized based on acceptance criteria provided in class.
+
+
+![Deployed site for portfolio](./Assets/images/scheduler-screenshot.png)
+
+Link to deployed application:
+
+https://christinastarr19.github.io/gt-scheduler-third-party-apis-homework-05/
+
+## Credits
+
+This was a solo project completed as homework for the Georgia Tech, FullStack Flex program.
+
+
+
+## MIT_License
+
+Copyright (c) [2020] [Christina Cunningham Starr]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
